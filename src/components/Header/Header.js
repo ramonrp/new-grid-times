e5d2f0feb764ce936454cf6ecebc6e7fe1805be5
Ramon Ruiz <ramon.ruizperez@gmail.com>
2021-07-27 15:18:14 +0200

    Excercise 1 completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,7 +29,21 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <LeftActionGroup>
+              <button>
+                <Search size={24} />
+              </button>
+              <button>
+                <Menu size={24} />
+              </button>
+        </LeftActionGroup>
         <Logo />
+        <RightActionGroup>
+              <InsideDiv>
+                <Button>SUBSCRIBE</Button>
+                <Anchor href="#">Already a subscriber?</Anchor>
+              </InsideDiv>    
+        </RightActionGroup>
       </MainHeader>
     </header>
   );
@@ -39,6 +53,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.desktopAndUp} {
+    display:none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +77,62 @@ const ActionGroup = styled.div`
   }
 `;
 
+const LeftActionGroup = styled.div`
+  display:none;
+
+  @media ${QUERIES.desktopAndUp} {
+    flex:1;
+    display:flex;
+    gap:24px;
+  }
+  
+`
+
+const RightActionGroup = styled.div`
+  display:none;
+
+  @media ${QUERIES.desktopAndUp} {
+    flex:1;
+    display:flex;
+    justify-content:flex-end;
+  }
+`
+
+const InsideDiv = styled.div`
+    
+    display:flex;
+    flex-direction:column;
+    gap:8px;
+    align-items: center;
+`
+const Anchor = styled.a`
+  text-decoration:underline;
+  font-style: italic;
+  font-size:${14/16}rem;
+  color: var(--grey-900);
+  &:hover{
+    text-decoration:none;
+    transform: scale(1.05)
+  }
+`
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    margin-top: 48px;
+    margin-bottom: 84px;
+  }
+  @media ${QUERIES.desktopAndUp} {
+    justify-content:center;
+    align-items:baseline;
+    margin-top: 16px;
+    margin-bottom: 86px;
+  }
 `;
 
 export default Header;
